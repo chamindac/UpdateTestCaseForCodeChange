9784106904cde48f7cc1d3e2112059c54fd788c9
chamindac <chaminda_chandrasekara@yahoo.com>
2018-12-04 15:03:25 +0800

    Enable detection of atributes and summary descriptions

--- TestCaseUpdater.ps1
+++ TestCaseUpdater.ps1
@@ -1,8 +1,64 @@
-$asm = [System.Reflection.Assembly]::LoadFrom('C:\Chaminda\tmp\drop\Testautomationproj1.dll')
-$classes =  $asm.GetTypes()
+$folderPath = $env:TestAssemblyPath;
+$testAssemblyFilter = $env:TestAssemblyFilePattern;
 
-foreach($class in $classes)
+$testAssemblies = (Get-ChildItem -Path $path -Recurse -Filter $testAssemblyFilter).FullName
+
+foreach($testAssembly in $testAssemblies)
 {
-    $class.Name
-    $methods = $class.GetMethods()
+    Write-Host ("******************************************************************")
+    Write-Host ("Test assembly found: " + $testAssembly)
+    Write-Host ("------------------------------------------------------------------")
+
+    $asm = [System.Reflection.Assembly]::LoadFrom($testAssembly)
+
+    $testDocumentationFilePath = $testAssembly -replace ".dll",".xml"
+    [xml]$assemblyDocument = Get-Content -Path $testDocumentationFilePath
+    
+    $classes =  $asm.GetTypes()
+    foreach($class in $classes)
+    {
+        Write-Host ("Inspecting class: " + $class.Name)
+        Write-Host ("------------------------------------------------------------------")
+        
+        $methods = $class.GetMethods()
+
+        foreach($method in $methods)
+        {
+            # Finding test methods from assembly
+            if (($method.CustomAttributes.Count -gt 0) -and  ($method.CustomAttributes.AttributeType.Name.Contains("TestMethodAttribute")))
+            {
+                Write-Host ("Test Method found: " + $method.Name)
+
+                # Finding the test case id of test method from properties
+                if (($method.CustomAttributes.AttributeType.Name.Contains("TestPropertyAttribute")) -and ($method.CustomAttributes.Where({$_.AttributeType.Name -eq "TestPropertyAttribute"}).ConstructorArguments[0].Value -eq "TestcaseID"))
+                {
+                    $testCaseId = $method.CustomAttributes.Where({$_.AttributeType.Name -eq "TestPropertyAttribute"}).ConstructorArguments[1].Value
+                    Write-Host ("TestcaseID:" + $testCaseId + " is found for Test Method: " + $method.Name)
+
+                    # Find any test category attributes if any
+                    if ($method.CustomAttributes.AttributeType.Name.Contains("TestCategoryAttribute"))
+                    {
+                       $testCategoryAtributes = $method.CustomAttributes.Where({$_.AttributeType.Name -eq "TestCategoryAttribute"});
+
+                       foreach($testCategoryAtribute in $testCategoryAtributes)
+                       {
+                            $testCategory = $testCategoryAtribute.ConstructorArguments[0];
+                            Write-Host ("Found Test Category: " + $testCategory + ". Adding to tags of test case ...")
+                       }
+                    }
+                    if ($assemblyDocument.doc.members.ChildNodes.Name.Contains(('M:' + $method.DeclaringType.FullName + "." + $method.Name)))
+                    {
+                       $testCaseSummary = $assemblyDocument.doc.members.ChildNodes.Where({$_.Name -eq ('M:' + $method.DeclaringType.FullName + "." + $method.Name)}).Summary
+
+                       Write-Host ("Found Test Case Summary: " + $testCaseSummary + ". Updating test case ...")
+                    }
+                }
+                else
+                {
+                    Write-Warning ("Test Method: " + $method.Name + " has not been defined with Test Property Attribute TestcaseID")
+                }
+            }
+        }
+        Write-Host ("------------------------------------------------------------------")
+    }
 }
